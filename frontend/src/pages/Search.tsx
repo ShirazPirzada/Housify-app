479c995f714f8f8802dffc44d928461259352b79
ShirazPirzada <shirazxpirzada@gmail.com>
2024-01-07 10:48:31 +0500

    Added Search Results Card

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,23 +1,47 @@
 import { BsCheckLg } from "react-icons/bs";
 import { useSearchContext } from "../contexts/SearchContext";
 import { useQuery } from "react-query";
-import * as apiClient from "../api-client"
+import * as apiClient from "../api-client";
 import { useState } from "react";
+import SearchResultCard from "../components/SearchResultsCard";
 const Search = () => {
   const search = useSearchContext();
-    const [page,setPage] = useState<number>(1);
-    const searchParams = {
-        destination: search.destination,
-        rentStartDate: search.rentStartDate.toISOString(),
-        rentEndDate:search.rentEndDate.toISOString(),
-        tenantCount:search.tenantCount.toString(),
-        page:page.toString(),
-    }
+  const [page, setPage] = useState<number>(1);
+  const searchParams = {
+    destination: search.destination,
+    rentStartDate: search.rentStartDate.toISOString(),
+    rentEndDate: search.rentEndDate.toISOString(),
+    tenantCount: search.tenantCount.toString(),
+    page: page.toString(),
+  };
 
   const { data: apartmentData } = useQuery(
     ["searchApartments", searchParams],
     () => apiClient.searchApartments(searchParams)
   );
-  return <>Search Page</>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+        <div className="space-y-5">
+          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter by:
+            {/* TODO Filters */}
+          </h3>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+        <span className="text-xl font-bold">
+          {apartmentData?.pagination.total} Apartments found
+          {search.destination ? ` in ${search.destination}`:""}
+        </span>
+        {/* TODO SORT options */}
+        </div>
+        {apartmentData?.data.map((apartment)=>(
+          <SearchResultCard apartment={apartment}/>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Search;
