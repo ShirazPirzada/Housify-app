e49f1d8ee38c4f18b624f9fb24e863dc01f2a5d3
ShirazPirzada <shirazxpirzada@gmail.com>
2024-03-03 21:01:15 +0500

    Updated types for apartments

--- frontend/src/config/apartment-options-config.ts
+++ frontend/src/config/apartment-options-config.ts
@@ -1,2 +1,2 @@
-export const apartmentTypes = ["Bachelor", "Family"];
+export const apartmentTypes = ["Bachelor", "Family","Both"];
 export const apartmentFacilities = ["Free Wifi","Parking","Water 24/7","Multi Meter Connection","Gas Connection","24/7 electricity"]
